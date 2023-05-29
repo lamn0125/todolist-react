dc46806e0b3525afb7999c1938299044f4a6bdef
lamn0125 <lamn0125@gmail.com>
2023-05-29 17:14:58 +0800

    todolist final

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -6,8 +6,51 @@ import {
 } from 'components/common/auth.styled';
 import { ACLogoIcon } from 'assets/images';
 import { AuthInput } from 'components';
+import { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useAuth } from '../contexts/AuthContext';
+import Swal from 'sweetalert2';
 
 const LoginPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const { login, isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate('/todos');
+    }
+  }, [navigate, isAuthenticated]);
+
+  const handleClick = async () => {
+    if (username.length === 0 || password.length === 0) {
+      return;
+    }
+    const success = await login({
+      username,
+      password,
+    });
+
+    if (success) {
+      Swal.fire({
+        position: 'top',
+        title: '登入成功！',
+        timer: 1000,
+        icon: 'success',
+        showConfirmButton: false,
+      });
+      return;
+    }
+    Swal.fire({
+      position: 'top',
+      title: '登入失敗！',
+      timer: 1000,
+      icon: 'error',
+      showConfirmButton: false,
+    });
+  };
+
   return (
     <AuthContainer>
       <div>
@@ -16,14 +59,28 @@ const LoginPage = () => {
       <h1>登入 Todo</h1>
 
       <AuthInputContainer>
-        <AuthInput />
+        <AuthInput
+          label="帳號"
+          type="text"
+          value={username}
+          placeholder="Enter username"
+          onChange={(nameInput) => setUsername(nameInput)}
+        />
       </AuthInputContainer>
 
       <AuthInputContainer>
-        <AuthInput />
+        <AuthInput
+          label="密碼"
+          type="password"
+          value={password}
+          placeholder="Enter password"
+          onChange={(passwordInput) => setPassword(passwordInput)}
+        />
       </AuthInputContainer>
-      <AuthButton>登入</AuthButton>
-      <AuthLinkText>註冊</AuthLinkText>
+      <AuthButton onClick={handleClick}>登入</AuthButton>
+      <Link to="/signup">
+        <AuthLinkText>註冊</AuthLinkText>
+      </Link>
     </AuthContainer>
   );
 };
